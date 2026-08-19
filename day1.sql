24036768266e23a2e49203d1761f0448aaf56c01
Sudharsun Sakthivel <133194476+sudharsun2005@users.noreply.github.com>
2026-08-19 13:11:06 +0530

    Add files via upload

--- day1.sql
+++ day1.sql
@@ -1,34 +1,110 @@
-create database practice;
-show databases;
-select database();
-use sakila;
-show tables;
-select * from city;
-select city_id,city from city;
-use practice;
-show tables;
-create table learner (sno int,name varchar(20));
-desc learner;
-select * from learner;
-insert into learner values (1,'praveen');
-insert into learner values (2,'vignesh');
-insert into learner values (3,'sudharsun');
-show tables;
-create database BankingDB;
-use BankingDB;
+-- 1. CREATE DATABASE
+CREATE DATABASE BankingDB;
+
+-- 2. USE DATABASE
+USE BankingDB;
+
+-- 3. CUSTOMERS TABLE
 
 
-DAY-2
-
-create table customer(
-	CustomerID INT,
-	FirstName VARCHAR(50),
-	LastName VARCHAR(50),
-	Email VARCHAR(100),
-	Phone VARCHAR(15)
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(20),
+    AccountCreationDate DATE,
+    DateOfBirth DATE
 );
 
-desc customer;
+-- 4. BRANCHES TABLE
 
+CREATE TABLE Branches (
+    BranchID INT PRIMARY KEY,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+-- 5. ACCOUNTS TABLE
+
+
+CREATE TABLE Accounts (
+    AccountID INT PRIMARY KEY,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2),
+    CustomerID INT,
+    BranchID INT,
+
+    CONSTRAINT chk_MinBalance
+    CHECK (Balance >= 1000),
+
+    CONSTRAINT FK_Accounts_Customers
+    FOREIGN KEY (CustomerID)
+    REFERENCES Customers(CustomerID),
+
+    CONSTRAINT FK_Accounts_Branches
+    FOREIGN KEY (BranchID)
+    REFERENCES Branches(BranchID)
+);
+
+-- 6. TRANSACTIONS TABLE
+
+
+CREATE TABLE Transactions (
+    TransactionID INT PRIMARY KEY,
+    AccountID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20),
+
+    CONSTRAINT FK_Transactions_Accounts
+    FOREIGN KEY (AccountID)
+    REFERENCES Accounts(AccountID)
+);
+
+-- 7. LOANS TABLE
+
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY,
+    CustomerID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE,
+
+    CONSTRAINT FK_Loans_Customers
+    FOREIGN KEY (CustomerID)
+    REFERENCES Customers(CustomerID)
+);
+
+
+-- 8. CHECK ALL TABLES
+
+DESC Customers;
+
+DESC Branches;
+
+DESC Accounts;
+
+DESC Transactions;
+
+DESC Loans;
+
+
+-- 9. SHOW ALL TABLES
+
+SHOW TABLES;
+
+
+
+select * from Customers;
+desc Customers;
+
+alter table Customers add column pan varchar(20);
+alter table Customers add column pan1 varchar(20)first;
+alter table Customers add column pan2 varchar(20)after email;
+alter table Customers drop column pan;
+alter table Customers modify column pan1 varchar(50);
 
 
